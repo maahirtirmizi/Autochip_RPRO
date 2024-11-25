3698857a8d3d3d9b1c6092b2e20317734072e846
maahirtirmizi <146031146+maahirtirmizi@users.noreply.github.com>
2024-11-25 10:39:25 -0800

    Delete verilogeval_prompts_tbs directory

--- verilogeval_prompts_tbs/vector100r/vector100r.sv
+++
@@ -1,6 +0,0 @@
-// Given a 100-bit input vector [99:0], reverse its bit ordering.
-
-module top_module (
-	input [99:0] in,
-	output reg [99:0] out
-);
--- verilogeval_prompts_tbs/vector2/vector2.sv
+++
@@ -1,6 +0,0 @@
-// Build a circuit that reverses the byte order of a 32-bit vector.
-
-module top_module (
-	input [31:0] in,
-	output [31:0] out
-);
